36b873cf9fc37d89b7b3d3d0f915c71af34da4e0
NaveenKumar <pnk0006@gmail.com>
2019-10-31 16:25:53 +0800

    Commit Login Page

--- dolphin-exchange/src/main.js
+++ dolphin-exchange/src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false
-
+const messages = {
+	ch: require("./common/language/ch"),
+	en: require("./common/language/en")
+};
 App.mpType = 'app'
 
 const app = new Vue({
